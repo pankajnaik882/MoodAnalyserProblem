b0f3badaeaee975f0fe62d1bdf3d96a14c8cd63b
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:57:52 +0530

    Add : Added Program.cs File

--- Program.cs
+++ Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+       
         public static void Main(string[] args)
         {
+            Program.ErrorFun();
+        }
 
+        public static void ErrorFun()
+        {
+
+            Console.WriteLine("Enter the Mood Happy or Sad: ");
+            string mood = Console.ReadLine();
+            try
+            {
+                if (mood == "Happy")
+                {
+                    Console.WriteLine("Happy");
+                }
+                if(mood=="Sad")
+                {
+                    Console.WriteLine("Sad");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
