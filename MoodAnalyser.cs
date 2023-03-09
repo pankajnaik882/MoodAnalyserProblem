4ba689dde39b3634fb34af006743053cf7ca1888
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:44:14 +0530

    Add : Analyse Response Mood as happy or sad

---
+++ MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    public class MoodAnalyser
+    {
+        public string Mood(string m)
+        {
+            if(m == "Happy")
+            {
+                return "HAPPY";
+            }
+            else
+            {
+                return "SAD";
+            }
+        }
+    }
+}
